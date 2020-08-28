cf40570fc18ca3203cdb4a533a8a330b7890c5a2
e-rahernandez <e-rahernandez@neoris.com>
2020-08-28 13:02:14 -0500

    Homework 1

---
+++ src/test/java/com/springframework5/webapplication/WebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework5.webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
